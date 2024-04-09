ba84cf328879316afe06d5b0d9b8adda1ea73b0e
Busisiwetshongozi <busisiwetshongozi@gmail.com>
2024-04-09 16:08:43 +0200

    inheritance

--- private_scopes.js
+++ private_scopes.js
@@ -1,5 +1,5 @@
 class person{
-    constructor(name){
+    constructor(fname){
         this.fname = fname
 
     }
@@ -10,26 +10,22 @@ class person{
 class student extends person{
     constructor(fname,id){
         super(fname);
-        this.id = id;}
-}
-     toString(){
+        this.id = id;
+    }
+    toString() {
         return(`${super.toString()}
-     with student id : ${this.id}`)
+        with student id : ${this.id}`)
+     
 
      }
+
+
+}
+    
         
     
 
      
-    
-
-
-
-    
-    
-
-
-
 let student1= new student('alice',28373878);
 console.log(student1);
 console.log(student1.toString());
